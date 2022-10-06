e57cebb08f67ddfea9542df343f0caeb0eee2252
yooske <yooske0214@gmail.com>
2022-10-06 05:48:58 +0000

    [add] header/footer,Top.Aboutページ

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_up_path_for(resource)
+    public_end_users_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, :introduction, :is_deleted])
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,27 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
+  def after_sign_out_path_for(resource)
+    new_end_user_session_path
+  end
+  
+  protected
+  
+  def reject_customer
+    @end_user = EndUser.find_by(email: params[:end_user][:email])
+    if @end_user
+      if @end_user.valid_password?(params[:end_user][:password]) && (@end_user.is_deleted == false)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to new_end_user_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
+      end  
+    end
+  end  
   # GET /resource/sign_in
   # def new
   #   super
