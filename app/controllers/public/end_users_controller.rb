db9e75574526bbd2d96dcfd958b0065a0f384582
yooske <yooske0214@gmail.com>
2022-10-07 05:57:42 +0000

    [add] enduserの会員情報詳細・変更画面と更新機能作成

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,16 +1,37 @@
 class Public::EndUsersController < ApplicationController
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
   end
 
   def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to public_end_user_path(@end_user)
+    else
+      render :edit
+    end
   end
 
   def confirm
   end
 
   def withdraw
+    @end_user = EndUser.find(params[:id])
+    # is_deletedカラムをtrueに変更することで削除フラグを立てる
+    @end_user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行致しました"
+    redirect_to root_path
   end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:profile_image, :name, :nickname, :email, :introduction, :is_deleted)
+  end
+
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,10 +3,20 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one_attached :profile_image
 
   validates :name, presence: true
   validates :nickname, presence: true
   validates :introduction, presence: true
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 
   def active_for_authentication?
     super && (is_deleted == false)
