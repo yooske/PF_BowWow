2d4593c9eed0ad58851c1d86b2a75d85eeb107aa
yooske <yooske0214@gmail.com>
2022-10-11 16:35:27 +0000

    [add,update] DM機能の追加,プロフィール画像のレイアウト編集

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,4 +1,31 @@
 class Public::ChatsController < ApplicationController
   def show
+    @end_user = EndUser.find(params[:id])
+    rooms = current_end_user.user_rooms.pluck(:room_id)
+    user_rooms = UserRoom.find_by(end_user_id: @end_user.id, room_id: rooms)
+
+    #user_roomがない場合、作成する
+    unless user_rooms.nil?
+      @room = user_rooms.room
+    else
+      @room = Room.new
+      @room.save
+      UserRoom.create(end_user_id: current_end_user.id, room_id: @room.id)
+      UserRoom.create(end_user_id: @end_user.id, room_id: @room.id)
+    end
+    @chats = @room.chats
+    @chat = Chat.new(room_id: @room.id)
   end
+
+  def create
+    @chat = current_end_user.chats.new(chat_params)
+    render :validater unless @chat.save
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:message, :room_id)
+  end
+
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -14,6 +14,9 @@ class EndUser < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :user_rooms, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_many :rooms, through: :user_rooms
 
   has_one_attached :profile_image
 
@@ -21,12 +24,12 @@ class EndUser < ApplicationRecord
   validates :nickname, presence: true
   validates :introduction, presence: true
 
-  def get_profile_image(width, height)
+  def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    profile_image
   end
 
   def active_for_authentication?
