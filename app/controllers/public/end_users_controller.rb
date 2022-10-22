3c1cfc5092dfc49d81f5b5270dcb22b69122f755
yooske <yooske0214@gmail.com>
2022-10-22 08:27:45 +0000

    [update] end_users_controller edit

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -39,15 +39,7 @@ class Public::EndUsersController < ApplicationController
 
   def posts
     @end_user = EndUser.find(params[:id])
-    @posts = @end_user.posts.page(params[:page]).per(6)
-    #タグを選択して関連する投稿を表示する
-    if params[:tag_ids]
-      tag_names = []
-      params[:tag_ids].each do |key, value|
-        tag_names << key if value == "1"
-      end
-      @posts = @end_user.posts.joins(:tags).where(tags:{name: tag_names}).distinct.page(params[:page]).per(6)
-    end
+    @posts = @end_user.posts.order(created_at: :desc).page(params[:page]).per(6)
   end
 
   private
